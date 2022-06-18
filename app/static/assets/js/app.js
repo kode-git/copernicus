c84876f67a54de979f2413af3c862824d1959c8d
Prop4et <francostanco97@gmail.com>
2022-06-18 13:13:02 +0200

    added request for prediction

--- app/static/assets/js/app.js
+++ app/static/assets/js/app.js
@@ -108,65 +108,21 @@ var markerClusters = new L.MarkerClusterGroup({
   maxClusterRadius: 60
 });
 
+function hex2rgb(colour, alpha) {
+  var r,g,b;
+    if ( colour.charAt(0) == '#') {
+        colour = colour.substr(1);
+  }
 
-function drawPlots(msg){
+  r = colour.charAt(0) + colour.charAt(1);
+  g = colour.charAt(2) + colour.charAt(3);
+  b = colour.charAt(4) + colour.charAt(5);
+
+  r = parseInt( r,16 );
+  g = parseInt( g,16 );
+  b = parseInt( b ,16);
+  return 'rgba(' + r + ',' + g + ',' + b + ','+ alpha + ')';
 }
-
-var years = [2011, 2012, 2013, /*2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021*/]
-const chart_div = document.getElementById('chart-div')
-function plot(coords) {
-  $.ajax({
-      type: "GET",
-      url: "/plot?coords="+coords,
-      dataType: "json",
-      success: function(msg) {
-        for(var year in msg.data){ 
-          var canvas = document.createElement("canvas");
-          canvas.id = "lineChart-"+year;
-          canvas.classList = "chart";
-          chart_div.appendChild(canvas);
-          var ctxL = canvas.getContext('2d');
-          new Chart(ctxL, {
-            type: 'line',
-            data: {
-              labels: Array.from(new Array(msg.data[year].length),(val,index)=> index),
-              datasets: [{
-                label: year,
-                data: msg.data[year],
-                backgroundColor: [
-                  'rgba(105, 0, 132, .2)',
-                ],
-                borderColor: [
-                  'rgba(200, 99, 132, .7)',
-                ],
-                borderWidth: 1,
-                pointRadius: 0
-              }/*,
-              {
-                label: "My Second dataset",
-                data: [28, 48, 40, 19, 86, 27, 90],
-                backgroundColor: [
-                  'rgba(0, 137, 132, .2)',
-                ],
-                borderColor: [
-                  'rgba(0, 10, 130, .7)',
-                ],
-                borderWidth: 2
-              }*/
-              ]
-            },
-            options: {
-              responsive: false
-            }
-          });
-        }
-      },
-      error: function (xhr, status, error) {
-          console.log(error);
-      }
-  });
-}
-
 
 var riverColors = {'Fiume Po': "#ff3135", 'Po': "#ff3135",
                    'Adige - Etsch': "#009b2e", 'Fiume Adige': '#009b2e', 'Adige': '#009b2e',
@@ -179,6 +135,75 @@ var riverColors = {'Fiume Po': "#ff3135", 'Po': "#ff3135",
                    'Piave': '#969696',
                    'Fiume Reno': '#0099ff'}
 
+const nameMatch ={'Fiume Po': 'Po', 'Po': 'Po',
+                  'Adige - Etsch': 'Adige', 'Fiume Adige': 'Adige', 'Adige': 'Adige',
+                  'Tevere': 'Tevere', 'Fiume Tevere': 'Tevere',
+                  'Adda': 'Adda', 'Fiume Adda':'Adda',
+                  'Oglio': 'Oglio', 'Fiume Oglio': 'Oglio',
+                  'Tanaro': 'Tanaro', 'Fiume Tanaro': 'Tanaro',
+                  'Ticino': 'Ticino', 'Fiume Ticino': 'Ticino',
+                  'Arno': 'Arno',
+                  'Piave': 'Piave',
+                  'Fiume Reno': 'Reno'
+}
+
+const values = ['dis', 'temp', 'prec'];
+const chart_div = document.getElementById('chart-div')
+var selected_coords = null;
+
+function plot(coords, name) {
+  var year = 2011 //default year
+  var year = $('#year').val()
+  $('#chart-div').empty();
+  $.ajax({
+      type: "GET",
+      url: "/plot?coords="+coords+'&year='+year,
+      dataType: "json",
+      success: function(msg) {
+        for(var i = 0; i < values.length; i++){ 
+          var canvas = document.createElement("canvas");
+          var div = document.createElement("div");
+          div.classList = 'divLineChart';
+          canvas.id = 'lineChart-'+year+'-'+i;
+          canvas.classList = "chart";
+          div.appendChild(canvas);
+          chart_div.appendChild(div);
+          var ctxL = canvas.getContext('2d');
+          new Chart(ctxL, {
+            type: 'line',
+            data: {
+              labels: msg.data[year]['time'],
+              datasets: [{
+                label: values[i] + ' ' + year,
+                data: msg.data[year][values[i]],
+                backgroundColor: [
+                  hex2rgb(riverColors[name], 0.5),
+                ],
+                borderColor: [
+                  riverColors[name],
+                ],
+                borderWidth: 1,
+                pointRadius: 0
+              }]
+            },
+            options: {
+              responsive: true,
+              scales: {
+                xAxes: [{
+                  ticks: {
+                    maxTicksLimit: 4
+                  }
+                }]
+              }
+            }
+          });
+        }
+      },
+      error: function (xhr, status, error) {
+          console.log(error);
+      }
+  });
+}
 
 var measuresLayer = L.geoJson(null);
 var measures = L.geoJson(null, {
@@ -196,15 +221,13 @@ var measures = L.geoJson(null, {
   },
   onEachFeature: function (feature, layer) {
     if (feature.name) {
-      var content = "<tr><th>River</th><td>" + feature.name + "</td></tr>" ;
+      var content = "<tr><th>River</th><td>" + nameMatch[feature.name] + "</td></tr>\
+                     <tr><th>Coords</th><td><" + feature.geometry.coordinates[1].toFixed(3) +','+ feature.geometry.coordinates[0].toFixed(3) +"></td></tr>" ;
       layer.on({
         click: function (e) {
-          syncSidebar(content)
-          plot(feature.geometry.coordinates)
-          /*$("#feature-title").html(feature.properties.NAME);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));*/
+          selected_coords = feature.geometry.coordinates;
+          syncSidebar(content);
+          plot(feature.geometry.coordinates, feature.name);
         }
       });
       
@@ -216,9 +239,6 @@ $.getJSON("static/data/points_new.geojson", function (data) {
   measures.addData(data);
   map.addLayer(measuresLayer);
 });
-
-
-
 
 map = L.map("map", {
   //zoom: 10,
@@ -351,3 +371,68 @@ function resize(e) {
     diff = 300;
   sidebar.style.width = diff + 'px';
 }
+
+const pred_div = document.getElementById('chart-div-pred');
+
+$('#days').click( () => {
+    $('#days-value').innerHTML = $('#days').val();
+    document.getElementById('days-value').innerHTML = $('#days').val();
+})
+
+$('#days').mousedown( () => {
+  $('#days').mousemove( () => {
+    $('#days-value').innerHTML = $('#days').val();
+    document.getElementById('days-value').innerHTML = $('#days').val();
+  })
+})
+
+$('#days').mouseup( () => {
+  $('#days').off('mousemove');
+})
+
+$('#pred').click( () => {
+  if(!selected_coords)
+    return;
+  $('#chart-div-pred').empty();
+  var days = $('#days').val();
+  if(!days || days === '-' || days === 0)
+    return;
+  $.post('/predict/'+days, (msg) => {
+    var canvas = document.createElement("canvas");
+    var div = document.createElement("div");
+    div.classList = 'divLineChart';
+    canvas.id = 'lineChartPred';
+    canvas.classList = "chart";
+    div.appendChild(canvas);
+    pred_div.appendChild(div);
+    var ctxL = canvas.getContext('2d');
+    new Chart(ctxL, {
+      type: 'line',
+      data: {
+        labels: msg.data['days'],
+        datasets: [{
+          label: 'prediction',
+          data: msg.data['pred'],
+          backgroundColor: [
+            hex2rgb('#ffcc80', 0.5),
+          ],
+          borderColor: [
+            '#ff9900',
+          ],
+          borderWidth: 1,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          xAxes: [{
+            ticks: {
+              maxTicksLimit: 4
+            }
+          }]
+        }
+      }
+    });
+  })
+})
